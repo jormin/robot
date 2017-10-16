0bdd65efb4be724761b9423fd74033d67fc301a2
Jormin <jorminxee@gmail.com>
2017-10-16 13:31:08 +0800

    cc

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -98,7 +98,9 @@
                         console.log("正在进行，请稍候");
                     },
                     success : function(msg) {
-                        if(msg.status === 1){
+                        __log(msg.status);
+                        __log(msg.msg);
+                        if(msg.status == 1){
                             layer.msg("录音上传成功");
                         }else{
                             layer.msg("录音上传失败");
