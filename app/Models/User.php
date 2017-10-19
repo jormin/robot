ceadbe59114c65267eb861dc0a2f30d426485159
Jormin <jorminxee@gmail.com>
2017-10-19 10:23:01 +0800

    cc

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,10 +53,10 @@ class User extends Authenticatable
      * 根据OPENID查询用户
      *
      * @param $openID
-     * @return array
+     * @return array|null
      */
     public static function getUserByOpenID($openID){
         $user = self::query()->where('openID', $openID)->first();
-        return $user->toArray();
+        return $user ? $user->toArray() : null;
     }
 }
